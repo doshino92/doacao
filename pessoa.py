9318237f5efe23b47ce05274cef056ef78307fb6
Guilherme Hino <guilherme_sh@hotmail.com>
2024-10-31 08:54:38 -0300

    implementado try except para validação de data no formato DD-MM-AAAA no setter do método data_nascimento. Add funções split e map(int) para mudar string para int e forçar receber a istring com traços

--- pessoa.py
+++ pessoa.py
@@ -16,4 +16,30 @@ class Pessoa:
     def nome(self, novo_nome: str):
         self.__nome = novo_nome
 
+    @property
+    def cpf(self):
+        return self.__cpf
 
+    @cpf.setter
+    def cpf(self, novo_cpf: str):
+        self.__cpf = novo_cpf
+
+    @property
+    def endereco(self):
+        return self.__endereco
+
+    @endereco.setter
+    def endereco(self, novo_endereco: str):
+        self.__endereco = novo_endereco
+
+    @property
+    def data_nascimento(self):
+        return self.__data_nascimento.strftime('%d/%m/%Y') #strftime  para data ser dia, mes e ano, ao inves de ano, mes e dia
+
+    @data_nascimento.setter
+    def data_nascimento(self, nova_data_nascimento: str):
+        try:
+            dia, mes, ano = map(int, nova_data_nascimento.split('-')) #split força a pessoa a coloca traço entre as datas
+            self.__data_nascimento = date(ano, mes, dia)              #map(int força string a virar um inteiro
+        except ValueError:
+            raise ValueError("A data deve estar entre traços Ex: DD-MM-AAAA")
