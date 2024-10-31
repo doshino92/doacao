12e03eea641964f368f173e1b61b08d92f99ec84
Guilherme Hino <guilherme_sh@hotmail.com>
2024-10-31 09:51:24 -0300

    implementada gets e sets classe Adotante

--- adotante.py
+++ adotante.py
@@ -1,4 +1,24 @@
 from pessoa import Pessoa
+
+
 class Adotante(Pessoa):
-    def __init__(self):
-        pass
+    def __init__(self,nome: str, cpf: str, endereco: str, ano: int, mes: int, dia: int, e_doador: bool, mora_apto: bool):
+        super().__init__(nome, cpf,endereco,ano, mes, dia)
+        self.__e_doador = e_doador
+        self.__mora_apto = mora_apto
+
+    @property
+    def e_doador(self):
+        return self.__e_doador
+
+    @e_doador.setter
+    def e_doador(self, novo_e_doador: bool):
+        self.e_doador = novo_e_doador
+
+    @property
+    def mora_apto(self):
+        return self.__mora_apto
+
+    @mora_apto.setter
+    def apto(self,novo_mora_apto: bool):
+        self.__mora_apto = novo_mora_apto
