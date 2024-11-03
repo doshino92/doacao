1eefe59a1eec780d7ecdeb0c38e5f723f89d8d84
Guilherme Hino <guilherme_sh@hotmail.com>
2024-11-03 15:14:47 -0300

    1 ajustado o método adocoes_por_periodo para funcionar corretamente

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from vacina import Vacina
 
 # criando adotantes
 adotante1 = Adotante('Marcos', '11221643606','Florianópolis',1992,9,19 )
-adotante2 = Adotante('Val', '333111333','Sao Jose',2000,2,13 )
+adotante2 = Adotante('Val', '333111333','Sao Jose',2010,2,13 )
 adotante3 = Adotante('Carol', '331345611333','Sao Jose',2000,2,13)
 
 # criando doadores
@@ -33,7 +33,7 @@ adocao1 = Adocao(2023,10,15,animal1, adotante1, doador1)
 adocao2 = Adocao(2022,2,28,animal2, adotante2, doador2)
 adocao3 = Adocao(2021,2,28,animal3, adotante3, doador3)
 
-
+#criando vacinas
 vacina1 = Vacina(tipos.TipoVacina.RAIVA, 2022, 2, 17)
 vacina2 = Vacina(tipos.TipoVacina.HEPATITE,2021,1,5)
 vacina3 = Vacina(tipos.TipoVacina.LEPTOSPIROSE,2020,2,8)
@@ -58,6 +58,7 @@ ong.registrar_adocao(adocao1)
 ong.registrar_adocao(adocao2)
 ong.registrar_adocao(adocao3)
 
+#listando doacoes por periodo
 ong.doacoes_por_periodo('01-01-1000', '31-12-2024')
 ong.adocoes_por_periodo("01-01-1021", "31-12-2022")
 
@@ -65,8 +66,11 @@ ong.adocoes_por_periodo("01-01-1021", "31-12-2022")
 #mostrando os animais disponíveis para Doção
 ong.animais_disponiveis()
 
+#vacinando animais
 ong.vacinar_animais_disponiveis(vacina1)
 ong.vacinar_animais_disponiveis(vacina2)
 ong.vacinar_animais_disponiveis(vacina3)
+
+#mostrando vacinas realizadas em um animal
 ong.mostrar_vacinas_animal(animal1)
 
